b1f31e139fc964dd8f59c8cce2bda4420a65f4f8
Justin Matsnev <jmatsnev@gmail.com>
2015-10-07 21:40:05 -0400

    pretty much done

---
+++ BrainQuiz/EndViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EndViewController.swift
+//  BrainQuiz
+//
+//  Created by Justin Matsnev on 10/7/15.
+//  Copyright © 2015 Justin Matsnev. All rights reserved.
+//
+
+import Cocoa
+
+class EndViewController: UIViewController {
+
+}
